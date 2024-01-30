90d467c5d89455f65313ec80bf08dff5a134f329
Charles Wight <chuck.wight@gmail.com>
2024-01-30 08:27:11 -0500

    fixed typo in iconUrl

--- src/main/java/com/respondog/LTIRequest.java
+++ src/main/java/com/respondog/LTIRequest.java
@@ -249,7 +249,7 @@ public class LTIRequest extends HttpServlet {
 			ofy().save().entity(a).now();
 			
 			String launchUrl = iss;
-			String iconUrl = iss.lastIndexOf("/")==iss.length()?iss+"images/respondog.png":iss+"/images/respondog.com";
+			String iconUrl = iss + (iss.lastIndexOf("/")==iss.length()?"":"/") + "images/respondog.png";
 			String client_id = d.client_id;
 			String subject = claims.get("sub").getAsString();
 			String nonce = Nonce.generateNonce();
